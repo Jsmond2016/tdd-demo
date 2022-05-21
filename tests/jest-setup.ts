e44831f8f760e50c9fd65aa9c9c16894dd0eaab0
Jsmond2016 <Jsmond2016@gmail.com>
2022-05-21 22:38:23 +0800

    feat: 配置 matchers，使用 toBeInTheDocument替代getByText

--- tests/jest-setup.ts
+++ tests/jest-setup.ts
@@ -19,6 +19,7 @@
 // 使用 Jest 的 Spy 和扩展 expect 来 Mock `window.location`
 import "jest-location-mock";
 import mockConsole from "jest-mock-console";
+import '@testing-library/jest-dom'
 
 mockConsole()
 
