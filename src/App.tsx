50f765cf1c1f0a59495f0f0400ad2314426fad46
Jsmond2016 <Jsmond2016@gmail.com>
2022-05-21 22:24:53 +0800

    chore: 组件测试-less文件报错无法通过测试

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import Title from "components/Title";
+import React from "react"
+import Title from "components/Title"
+import AuthButton from 'components/AuthButton'
 
 const App = () => {
   return (
     <div>
-      <section>
+      {/* <section>
         <Title type="small" title="小字" />
         <Title type="large" title="大字" />
+      </section> */}
+      <section>
+        <AuthButton>登录</AuthButton>
       </section>
     </div>
   )
 }
 
-export default App;
+export default App
