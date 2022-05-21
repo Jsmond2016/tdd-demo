53826d1cfa2105b4e78f126ec26df37dc1c341b7
Jsmond2016 <Jsmond2016@gmail.com>
2022-05-21 17:01:21 +0800

    feat: 发现问题-jest 无法处理 href 问题，jest 没有将 jsdom 放在全局

---
+++ src/utils/getSearchObj.ts
@@ -0,0 +1,29 @@
+
+/**
+ * 作用是 把网页地址中的 查询参数字符串 转换为 对象
+ * 
+ * @returns 
+ */
+const getSearchObj = () => {
+  // ?a=1&b=2
+  const { search } = window.location;
+
+  // a=1&b=2
+  const searchStr = search.slice(1);
+
+  // ['a=1', 'b=2']
+  const pairs = searchStr.split("&");
+
+  // { 'a': '1' }
+  const searchObj: Record<string, string> = {};
+
+  pairs.forEach((pair) => {
+    // [a, 1]
+    const [key, value] = pair.split("=");
+    searchObj[key] = value;
+  });
+
+  return searchObj;
+};
+
+export default getSearchObj;
