dd4e92014a5b71b1a0fa13b2504ad10dba99574f
Jsmond2016 <Jsmond2016@gmail.com>
2022-05-21 17:05:01 +0800

    fix: 使用第三方库处理 location 问题

--- tests/utils/getSearchObj.test.ts
+++ tests/utils/getSearchObj.test.ts
@@ -2,7 +2,10 @@ import getSearchObj from "utils/getSearchObj";
 
 describe("getSearchObj", () => {
   it("可以获取当前网址的查询参数对象", () => {
-    window.location.href = "https://www.baidu.com?a=1&b=2";
+    // 使用全局暴露出来的 jsdom
+    global.jsdom.reconfigure({
+      url: "https://www.baidu.com?a=1&b=2",
+    });
 
     expect(window.location.search).toEqual("?a=1&b=2");
     expect(getSearchObj()).toEqual({
@@ -12,7 +15,10 @@ describe("getSearchObj", () => {
   });
 
   it("空参数返回空", () => {
-    window.location.href = "https://www.baidu.com";
+    // 使用全局暴露出来的 jsdom
+    global.jsdom.reconfigure({
+      url: "https://www.baidu.com",
+    });
 
     expect(window.location.search).toEqual("");
     expect(getSearchObj()).toEqual({});
