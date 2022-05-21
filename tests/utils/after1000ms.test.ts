59d7d19a1b2ea3b07a2ceb4aa2c61c684ccd0582
Jsmond2016 <Jsmond2016@gmail.com>
2022-05-21 17:38:34 +0800

    feat: 方法2-使用快进的方式对定时器测试

--- tests/utils/after1000ms.test.ts
+++ tests/utils/after1000ms.test.ts
@@ -7,9 +7,16 @@ describe("after1000ms", () => {
 
   it("可以在 1000ms 后自动执行函数", () => {
     jest.spyOn(global, "setTimeout");
+    // 用 jest.fn 生成了一个监听函数（假函数）
+    const callback = jest.fn();
+    
+    expect(callback).not.toHaveBeenCalled();
 
-    after1000ms();
+    after1000ms(callback);
 
+    jest.runAllTimers();
+
+    expect(callback).toHaveBeenCalled();
     expect(setTimeout).toHaveBeenCalledTimes(1);
     expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 1000);
   });
