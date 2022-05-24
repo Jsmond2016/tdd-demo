765f6533794bc50f38d14258fb00b82ef92c6828
Jsmond2016 <Jsmond2016@gmail.com>
2022-05-24 21:36:36 +0800

    feat: 多次Mock-object.defineProperty 万能方案的使用

--- tests/utils/env/env.test.ts
+++ tests/utils/env/env.test.ts
@@ -1,23 +1,34 @@
 import * as envUtils from 'utils/env';
 
+// 最万能的方法 —— Object.defineProperty
+// 这样我们既不需要用 @ts-ignore 也不需要把 const 改成 let 了
+// --要注意的是，无论用直接赋值还是 Object.defineProperty，都需要在最开始记录 env 的值，
+// --然后加一个 afterEach 在执行每个用例后又赋值回去，否则会造成用例之间的污染！
+
 const originEnv = envUtils.env;
 
 describe("env", () => {
   afterEach(() => {
-    // @ts-ignore
-    envUtils.env = originEnv;
+    Object.defineProperty(envUtils, 'env', {
+      value: originEnv,
+      writable: true,
+    })
   })
-  
+
   it('开发环境', () => {
-    // @ts-ignore
-    envUtils.env = 'dev'
+    Object.defineProperty(envUtils, 'env', {
+      value: 'dev',
+      writable: true,
+    })
 
     expect(envUtils.env).toEqual('dev');
   })
 
   it('正式环境', () => {
-    // @ts-ignore
-    envUtils.env = 'prod'
+    Object.defineProperty(envUtils, 'env', {
+      value: 'prod',
+      writable: true,
+    })
 
     expect(envUtils.env).toEqual('prod');
   })
